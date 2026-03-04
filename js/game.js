2eae1eed8b31fe4b5742125d7740420934286d9f
NPausC <norman@popcorn-solution.de>
2026-03-04 09:08:24 +0100

    Updates:all new

--- js/game.js
+++ js/game.js
@@ -1,20 +1,12 @@
 let canvas;
 let ctx;
-let character = new Character();
+let world = new World()
 
 function init() {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
-    character.loadImage(ImageHub.Character.walk[0]);
 
-    ctx.drawImage(
-        character.img,
-        character.x,
-        character.y,
-        character.width,
-        character.height,
-    );
 
-    console.log('My character is', character)
-
+    
+    console.log("My character is", world.character);
 }
--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -2,17 +2,7 @@ class MovableObject {
     x = 120;
     y = 200;
     img;
-    width = 100;
-    height = 150;
 
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
-
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
 
     moveRight() {}
 
